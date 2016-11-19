65d2afdb466a5c3c218ebbe98771a0c14f392e65
Acrobat622 <86.ed.liu@gmail.com>
2016-11-19 19:20:09 +0800

    silly scripts

---
+++ mesi.sh
@@ -0,0 +1,14 @@
+for data in "blackscholes" "bodytrack" "fluidanimate";
+do 
+    for cache in 1024 8092 32768; 
+    do
+        for assoc in 1 2 4;
+        do
+            for block in 8 32 128;
+            do
+                echo $data $cache $assoc $block
+                ~/Downloads/pypy2-v5.6.0-osx64/bin/pypy simulator.py mesi $data $cache $assoc $block
+            done
+        done
+    done
+done
